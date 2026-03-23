dc84794228835c63d674e478db3744767355c693
Nodroggg <gordon.fenning@gmail.com>
2026-03-23 12:21:56 +0000

    Player CH3 complete

--- main.py
+++ main.py
@@ -9,7 +9,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+  
+
+    
 
     while True:
         log_state()
@@ -18,10 +27,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)        
+        updatable.update(dt)        
         
         screen.fill("black")
-        player.draw(screen)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
         
 
